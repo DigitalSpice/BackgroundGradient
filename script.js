cd657267a6bfd208936d89e1d20ed5cc8b956502
DigitalSpice <lmcurry93@hotmail.com>
2023-01-09 23:03:46 -0800

    Added Randomizer!

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 let CSS = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
+let randColor = document.querySelector(".randomize");
 let body = document.getElementById("gradient");
 let copyBtn = document.getElementById("copyBtn");
 let copyText = document.createElement("button");
 
 function setGradient(){
-    body.style.background = "linear-gradient(45deg, " 
-    + color1.value 
-    + ", " 
-    + color2.value
-   + ") no-repeat fixed" ;
+  body.style.background = "linear-gradient(45deg, " 
+  + color1.value 
+  + ", " 
+  + color2.value
+  + ") no-repeat fixed" ;
 
-   CSS.textContent = "Gradient: " + body.style.background + ";";
+  CSS.textContent = "Gradient: " + body.style.background + ";";
+}
+
+function randomHex(){
+  return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function randomizer(){
+  color1.value = randomHex();
+  color2.value = randomHex();
+  setGradient();
 }
 
 function copyButton(){
-    copyText.appendChild(document.createTextNode("Copy Code"));
-    copyBtn.appendChild(copyText);
-    copyBtn.addEventListener("click", function(){
-      navigator.clipboard.writeText(body.style.background + ";");
-      alert("Code Copied!");
-    })
+  copyText.appendChild(document.createTextNode("Copy Code"));
+  copyBtn.appendChild(copyText);
+  copyBtn.addEventListener("click", function(){
+    navigator.clipboard.writeText(body.style.background + ";");
+    alert("Code Copied!");
+  })
  }
  
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
 
+randColor.addEventListener("click",randomizer);
 copyButton();
+
+
+
 
 
